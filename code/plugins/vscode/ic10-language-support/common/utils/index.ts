bc3db7f3537a6304d82072d7ed6488feceb14510
edocsitahw <2257699870@qq.com>
2026-07-21 17:07:42 +0800

    chore: Adjust the rebase code to be the same as the develop branch

---
+++ code/plugins/vscode/ic10-language-support/common/utils/index.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2026. All rights reserved.
+ * This source code is licensed under the CC BY-NC-SA
+ * (Creative Commons Attribution-NonCommercial-NoDerivatives) License, By Xiao Songtao.
+ * This software is protected by copyright law. Reproduction, distribution, or use for commercial
+ * purposes is prohibited without the author's permission. If you have any questions or require
+ * permission, please contact the author: edocsitahw@qq.com
+ */
+
+/**
+ * @file index.ts
+ * @author edocsitahw
+ * @version 1.1
+ * @date 2026/06/28 13:01
+ * @desc
+ * @copyright CC BY-NC-SA 2026. All rights reserved.
+ * */
+export * from "./utils";
+export * from "./constant";
